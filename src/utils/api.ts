5b31a755ec97082b98326044530719d537d55ed1
Sefa <50076109+Seefaaa@users.noreply.github.com>
2024-03-30 17:34:07 +0300

    Verify

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -14,3 +14,19 @@ export async function get<T>(
 
 	return result.data;
 }
+
+export async function post<T>(
+	endpoint: string,
+	data: any,
+	secret = true
+): Promise<Response<T>> {
+	const result = await axios.post(
+		`${process.env.API_URL}/v${process.env.API_VERSION}/${endpoint}`,
+		data,
+		{
+			headers: secret ? { 'X-API-KEY': process.env.API_KEY } : undefined,
+		}
+	);
+
+	return result.data;
+}
